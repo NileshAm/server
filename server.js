88e684155d116279585ef3325249de5cf4e73ac0
Nilesh Amarathunge <nileshamarathunge@gmail.com>
2024-01-21 15:12:32 +0530

    firebase uploader made

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 
+const fileUpload = require("express-fileupload");
+const firebaseUploader = require("./Utils/FirebaseUploader")
+
 const PORT = process.env.PORT;
 
 const app = express();
 const cors = require("cors");
+
 
 app.use(
   cors({
@@ -12,6 +16,8 @@ app.use(
     methods: ["GET", "POST"],
   })
 );
+
+app.use(fileUpload());
 
 const connection = require("./Utils/DatabaseConnection").connectDB();
 const SQLExecuter = require("./Utils/SQLExecuter");
@@ -25,6 +31,27 @@ app.get("/in", (req, res) => {
 app.get("/product/:inqury", (req, res) => {
   if (req.params.inqury == "home") {
     SQLExecuter.query(res, connection, "SELECT * FROM Product");
+  }
+  if (req.params.inqury == "banner") {
+    SQLExecuter.query(res, connection, "SELECT * FROM Banners");
+  }
+});
+
+app.post("/api/img", (req, res) => {
+  if (req.files != null) {
+    file = req.files;
+    console.log(file.image);
+    firebaseUploader.FirebaseUpload(file.image).then((responce)=>{
+      // if (responce.code == 200){
+      //     res.json({ message: "Uploaded Succesfully", code: 200 });
+      // }else{
+      //     res.json({ message: "Error occured while converting or uploading", code: 500 });
+      // }
+      res.json(responce)
+    })
+    
+  } else {
+    res.json({ message: "No image received", code: 500 });
   }
 });
 
