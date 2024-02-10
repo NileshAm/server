5b5b5b4dfe3514854eaa39f165f80d0802d43555
Nilesh Amarathunge <nileshamarathunge@gmail.com>
2024-02-10 15:06:42 +0530

    /search end point modified to facilitate saerch queries

--- server.js
+++ server.js
@@ -106,19 +106,103 @@ app.get("/login/", (req, res) => {
   res.status(205).json({ User: req.params.user });
 });
 
-app.get("/test", (req, res)=>{
-  res.status(200).json({ State : "Working...", code : 200})
-})
+app.get("/test", (req, res) => {
+  res.status(200).json({ State: "Working...", code: 200 });
+});
 
-app.get("/product", async (req, res) => {
-  connection.query(`select * from Product where ID = ${req.query.id}` , (err, result)=>{
-    if(err){
-      res.status(500).json({message : {err}, code :500})
-    }else{
-      res.status(200).json(result)
+app.get("/product", (req, res) => {
+  connection.query(
+    `select * from Product where ID = ${req.query.id}`,
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ message: { err }, code: 500 });
+      } else {
+        res.status(200).json(result);
+      }
     }
+  );
+});
 
-  })
+app.get("/api/:type", (req, res) => {
+  const types = ["category", "brands"];
+  const type = req.params.type;
+  if (!types.includes(type)) {
+    res
+      .status(400)
+      .json({ message: `Path '/api/${type}' not found`, code: 400 });
+  }
+  connection.query(`select * from ${type} order by ID ASC`, (err, result) => {
+    if (err) {
+      res.status(500).json({ message: { err }, code: 500 });
+    } else {
+      if (req.query.format === "list") {
+        res.status(200).json(result);
+      } else if (req.query.format === "json") {
+        formatedJson = {};
+        result.forEach((element) => {
+          let keys = Object.keys(element);
+
+          formatedJson[element[keys[0]]] = element[keys[1]];
+        });
+        res.status(200).json(formatedJson);
+      } else {
+        res.status(400).json({
+          message: `Path '${req.path}?format=${req.query.format}' not found`,
+          code: 400,
+        });
+      }
+    }
+  });
+});
+
+app.post("/search", (req, res) => {
+  let data = req.body;
+
+  const intArray = (array) => {
+    let numArray = [];
+    array.forEach((element) => {
+      if (Number.isInteger(parseInt(element))) {
+        numArray.push(element);
+      }
+    });
+    return numArray;
+  };
+
+  data.brands = intArray(data.brands.split(",")).toString();
+  data.categories = intArray(data.categories.split(",")).toString();
+
+  let query = "select * from Product where ";
+
+  if (data.term !== "") {
+    query += `Name like '%${data.term}%' `;
+  }
+  if (data.price !== "") {
+    if (data.term !== "") {
+      query += "and ";
+    }
+    query += `Price <= ${data.price} `;
+  }
+  if (data.categories !== "") {
+    if (data.price !== "" || data.term !== "") {
+      query += "and ";
+    }
+    query += `Category in (${data.categories}) `;
+  }
+  if (data.brands !== "") {
+    if (data.price !== "" || data.term !== "" || data.categories !== "") {
+      query += "and ";
+    }
+    query += `Brand in (${data.brands}) `;
+  }
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.error("Error fetching data : " + err);
+      console.error(query);
+      res.status(500).json({ message: { err }, code: 500 });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 app.listen(PORT, () => {
